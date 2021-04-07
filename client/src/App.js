5e2bfafd5dc1f42aff87f2c45204bbcde93577c3
David Eldridge <d.vincenteldridge@gmail.com>
2021-04-07 14:23:00 -0400

    imported landing page, starting create account page

--- client/src/App.js
+++ client/src/App.js
@@ -1,57 +1,18 @@
 import React from 'react';
-import './App.css';
-import bg from "./img/SoccerHeader.mp4";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Landing from "./pages/Landing/Landing";
+import CreateAccount from "./pages/CreateAccount/CreateAccount";
+// import NoMatch from "./pages/NoMatch";
+// import Nav from "./components/Nav";
+
 
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <div className="showcase">
-          <div className="grid-container">
-            <div className="grid-x grid-margin-x align-center">
-              <form id="login-form" className="log-in-form" method="POST">
-                <h4 className="text-center">Log in with your username</h4>
-                <label >
-                  Username
-                  <input
-                    id="username-input-login"
-                    type="text"
-                    name="username"
-                    placeholder="Enter Username"
-                  />
-                </label>
-                <label >
-                  Password
-                  <input
-                    id="password-input-login"
-                    type="password"
-                    name="password"
-                    placeholder="Enter Password"
-                  />
-                </label>
-                <p>
-                  <input
-                    type="submit"
-                    className="button expanded"
-                    value="Log in"
-                  ></input>
-                </p>
-                <p className="text-center">
-                  <a href="/create-acc">No account? Sign up today!</a>
-                </p>
-              </form>
-              <video
-                className="showcase"
-                muted
-                loop
-                autoPlay
-              ><source src={bg} type="video/mp4" />
-              </video>
-            </div>
-            <div className="toggle"></div>
-          </div>
+        <div>
+            <Landing /> 
+            {/* <CreateAccount />       */}
         </div>
-      </div>
     );
   }
 }
