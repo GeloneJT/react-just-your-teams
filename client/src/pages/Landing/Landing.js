f8f90aed716a4a39e5189a2e1140fe5b19bb7b18
Jelani Thomas <jelani13@icloud.com>
2021-04-09 02:56:48 -0400

    Login component created

--- client/src/pages/Landing/Landing.js
+++ client/src/pages/Landing/Landing.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import './Landing.css';
+import React from "react";
+import "./Landing.css";
 import bg from "../../img/SoccerHeader.mp4";
 
 class Landing extends React.Component {
@@ -9,43 +9,9 @@ class Landing extends React.Component {
         <div className="showcase">
           <div className="grid-container">
             <div className="grid-x grid-margin-x align-center">
-              <form id="login-form" className="log-in-form" method="POST">
-                <h4 className="text-center">Log in with your username</h4>
-                <label >
-                  Username
-                  <input
-                    id="username-input-login"
-                    type="text"
-                    name="username"
-                    placeholder="Enter Username"
-                  />
-                </label>
-                <label >
-                  Password
-                  <input
-                    id="password-input-login"
-                    type="password"
-                    name="password"
-                    placeholder="Enter Password"
-                  />
-                </label>
-                <p>
-                  <input
-                    type="submit"
-                    className="button expanded"
-                    value="Log in"
-                  ></input>
-                </p>
-                <p className="text-center">
-                  <a href="/create-acc">No account? Sign up today!</a>
-                </p>
-              </form>
-              <video
-                className="showcase"
-                muted
-                loop
-                autoPlay
-              ><source src={bg} type="video/mp4" />
+              <LoginForm>{/* Handle form */}</LoginForm>
+              <video className="showcase" muted loop autoPlay>
+                <source src={bg} type="video/mp4" />
               </video>
             </div>
             <div className="toggle"></div>
