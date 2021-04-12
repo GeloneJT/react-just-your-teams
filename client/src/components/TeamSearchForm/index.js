9f482a367e9490d58849a4537696e79fc0dfcf74
Michael DiGiacomo <mdigiacomo05@me.com>
2021-04-12 16:49:05 -0400

    renamed file, and started team searhc form function

---
+++ client/src/components/TeamSearchForm/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function TeamSearchForm() {
+    return (
+        <form className="translucent-form-overlay" method="POST">
+            <h3>Search for Teams</h3>
+        </form>
+    )
+}
