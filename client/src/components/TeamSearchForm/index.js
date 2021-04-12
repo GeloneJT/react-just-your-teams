86119578b3462a999949f9d8257f71a34c147922
Michael DiGiacomo <mdigiacomo05@me.com>
2021-04-12 16:56:54 -0400

    brought over teams label

--- client/src/components/TeamSearchForm/index.js
+++ client/src/components/TeamSearchForm/index.js
@@ -16,6 +16,35 @@ function TeamSearchForm() {
           <option value="epl">English Premier League</option>
         </select>
       </label>
+      <label>
+        Teams
+        <select id="teams-input" name="status" type="text">
+          <option value="Arsenal FC">Arsenal FC</option>
+          <option value="Aston Villa FC">Aston Villa FC</option>
+          <option value="Brighton & Hove Albion FC">
+            Brighton & Hove Albion FC
+          </option>
+          <option value="Burnley FC">Burnley FC</option>
+          <option value="Chelsea FC">Chelsea FC</option>
+          <option value="Crystal Palace FC">Crystal Palace FC</option>
+          <option value="Everton FC">Everton FC</option>
+          <option value="Fulham FC">Fulham FC</option>
+          <option value="Leeds United">Leeds United</option>
+          <option value="Leicester City FC">Leicester City</option>
+          <option value="Liverpool FC">Liverpool FC</option>
+          <option value="Manchester City FC">Manchester City FC</option>
+          <option value="Manchester United FC">Manchester United FC</option>
+          <option value="Newcastle United FC">Newcastle United FC</option>
+          <option value="Sheffield United FC">Sheffield United FC</option>
+          <option value="Southampton FC">Southampton FC</option>
+          <option value="Tottenham Hotspur FC">Tottenham Hotspur</option>
+          <option value="West Bromwich Albion">West Bromwich Albion</option>
+          <option value="West Ham United FC">West Ham United FC</option>
+          <option value="Wolverhampton Wanderers FC">
+            Wolverhampton Wanderers
+          </option>
+        </select>
+      </label>
     </form>
   );
 }
