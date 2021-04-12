a5d330ac491c039d385b3189de55f40d0ea126bb
Michael DiGiacomo <mdigiacomo05@me.com>
2021-04-12 16:53:23 -0400

    Brought over the sport label

--- client/src/components/TeamSearchForm/index.js
+++ client/src/components/TeamSearchForm/index.js
@@ -1,9 +1,16 @@
 import React from "react";
 
 function TeamSearchForm() {
-    return (
-        <form className="translucent-form-overlay" method="POST">
-            <h3>Search for Teams</h3>
-        </form>
-    )
+  return (
+    <form className="translucent-form-overlay" method="POST">
+      <h3>Search for Teams</h3>
+      <label>
+        Sport
+        <select id="sport-input" name="status" type="text">
+          <option value="soccer">Soccer</option>
+        </select>
+      </label>
+      
+    </form>
+  );
 }
