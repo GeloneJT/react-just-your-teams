4ccfc9536d9821fdbc355e7b011ddb9102bc6a49
Jelani Thomas <jelani13@icloud.com>
2021-04-15 11:46:20 -0400

    passport set up at root

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
+const passport = require("passport");
+const { session } = require("passport");
 const PORT = process.env.PORT || 3001;
-
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -12,14 +13,22 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
+app.use(
+  session({
+    secret: "super secret",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(routes);
 
-
-
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/jytDB", 
-  {
-    useNewUrlParser: true,
-  });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/jytDB", {
+  useNewUrlParser: true,
+});
 
 app.listen(PORT, function () {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
