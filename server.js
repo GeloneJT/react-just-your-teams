2f05b84467732949812ff6681c7e51aaab5ae65c
James VanderKam <jamesvanderkam@verizon.net>
2021-04-15 10:56:07 -0400

    c=socket put into server?

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
+const io = require('socket.io')(http);
 const PORT = process.env.PORT || 3001;
 
 
@@ -19,6 +20,36 @@ app.use(routes);
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/jytDB", 
   {
     useNewUrlParser: true,
+  });
+
+
+  io.on('connection', (socket) => {
+
+    // Get the last 10 messages from the database.
+    Message.find().sort({createdAt: -1}).limit(10).exec((err, messages) => {
+      if (err) return console.error(err);
+  
+      // Send the last messages to the user.
+      socket.emit('init', messages);
+    });
+  
+    // Listen to connected users for a new message.
+    socket.on('message', (msg) => {
+      // Create a message with the content and the name of the user.
+      const message = new Message({
+        content: msg.content,
+        name: msg.name,
+        team:msg.team,
+      });
+  
+      // Save the message to the database.
+      message.save((err) => {
+        if (err) return console.error(err);
+      });
+  
+      // Notify all other users about a new message.
+      //socket.broadcast.emit('push', msg);
+    });
   });
 
 app.listen(PORT, function () {
